f9c845a3aec5d566faabb956b7a447435215969a
Sanskar Jain <sanskarjain1997.19@gmail.com>
2022-07-25 15:56:58 +0530

    uploaded to PyPI

--- src/gist_sync/__init__.py
+++
@@ -1,6 +0,0 @@
-from gist_sync.gist_sync import GistSync
-from . import cli
-
-__version__ = '0.1'
-__project__ = "gist-sync"
-__author__ = "Nishu Jain"
---
+++ src/repo_gist_sync/__init__.py
@@ -0,0 +1,6 @@
+from repo_gist_sync.repo_gist_sync import GistSync
+from . import cli
+
+__version__ = '0.1'
+__project__ = "repo-gist-sync"
+__author__ = "Nishu Jain"
